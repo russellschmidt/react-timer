910655bf016fce88aad53555465b6b7a1d0c8c90
Russell Schmidt <russell.schmidt@me.com>
2017-04-28 16:32:09 -0700

    modify server.js and package.json for push to heroku

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ var express = require('express');  // takes string name of module as argument
 
 // create our app
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+// make a fix for open weather map. We need to adjust https traffic to http
+app.use( function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 // tell it what folder to serve. app.use add functionality
 // express.static(folder) is the folder we are exposing
 app.use(express.static('public'));
 
 // start the server. takes 2 arguments, port and a function called on start
-app.listen(3000, function(){
-  console.log('Express server is up on port 3000');
+app.listen(PORT, function(){
+  console.log('Express server is up on port ' + PORT);
 });
