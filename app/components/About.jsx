9e1c5b737c504e7b2adaae01e9915bafc0d06a17
Russell Schmidt <russell.schmidt@me.com>
2017-05-01 15:34:51 -0700

    style about.jsx file

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,25 @@
 var React = require("react");
+var {Link} = require("react-router");
 
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>This is the about page</p>
+      <h1 className="text-centered">About</h1>
+      <p>Learn more about this little app</p>
+      <ol>
+        <li>
+          <a href="https://github.com/russellschmidt/react-weather">Visit the Github Repo</a>
+        </li>
+        <li>
+          <a href="https://www.russellschmidt.net">Visit Me</a>
+        </li>
+        <li>
+          <a href="https://www.twitter.com/rustiest">Find Me On Twitter</a>
+        </li>
+        <li>
+          <a href="https://www.udemy.com/the-complete-react-web-app-developer-course">Udemy Inspiration</a>
+        </li>
+      </ol>
     </div>
   )
 };
