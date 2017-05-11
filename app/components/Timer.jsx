d260092e8ed38741263592fa68ed5abcf83e6624
Russell Schmidt <russell.schmidt@me.com>
2017-05-11 13:32:14 -0700

    add sweet baby timer tests to the timer baby

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,13 +1,53 @@
-var React = require("react");
+var React = require ("react");
+var Clock = require ("Clock");
+var Controls = require ("Controls");
 
 var Timer = React.createClass({
+  getInitialState: function() {
+    return {
+      count: 0,
+      timerStatus: 'stopped'
+    };
+  },
+  componentDidUpdate: function (prevProps, prevState) {
+    if (this.state.timerStatus !== prevState.timerStatus) {
+      switch(this.state.timerStatus) {
+        case 'started':
+          this.handleStart();
+          break;
+        case 'stopped':
+          this.setState({count: 0});
+        case 'paused':
+          clearInterval(this.timer);
+          this.timer = undefined;
+          break;
+      }
+    }
+  },
+  componentWillUnmount: function () {
+    clearInterval(this.timer);
+  },
+  handleStatusChange: function (newTimerStatus) {
+    this.setState({timerStatus: newTimerStatus});
+  },
+  handleStart: function () {
+    this.timer = setInterval( () => {
+      this.setState({
+        count: this.state.count + 1
+      })
+    }, 1000);
+  },
   render: function () {
+    var {count, timerStatus} = this.state;
+
     return (
       <div>
-        <p>Timer.jsx</p>
+        <h1 className = "page-title">Timer</h1>
+        <Clock totalSeconds={count}/>
+        <Controls countdownStatus={timerStatus} onStatusChange={this.handleStatusChange}/>
       </div>
     )
   }
-})
+});
 
 module.exports = Timer;
